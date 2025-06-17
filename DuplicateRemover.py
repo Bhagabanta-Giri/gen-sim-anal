6de0d841c027c5cf0aae694fdc66f2a51d303c8e
Bhagabanta-Giri <bhagabantagiri@gmail.com>
2025-06-17 17:33:57 +0530

    All required changes made in AlleleCreator and DuplicateRemover

--- DuplicateRemover.py
+++ DuplicateRemover.py
@@ -1,7 +1,43 @@
-children = sample = [['A', 'A'], ['A', 'a'], ['A', 'b'], ['A', 'b'], ['a', 'a'], ['a', 'b'], ['a', 'b'], ['B', 'A'], ['B', 'a'], ['B', 'b'], ['B', 'b'], ['b', 'b'], ['b', 'b']]
-def duplicate_remover(sample, children):
-    for gene_up in sample:
-        for gene_down in sample:
-            if gene_up != gene_down and set(gene_up) == set(gene_down):
-                children.pop(sample.index(gene_down))
-    return children
+from random import choice
+
+
+while True:
+    user_inp = input("Enter a set of characters: ").replace(" ", "").replace("\t", "").replace("\n", "")
+    if user_inp.isalpha():
+        if len(user_inp) % 2 == 0:
+            break
+        else:
+            waste = choice(user_inp)
+            user_inp = user_inp.replace(waste, "")
+            print("Odd number of characters used. \nPurging a random character...")
+            break
+    else:
+        print("Invalid input")
+
+
+
+def sample_creator(given_string):
+    index_init = 2
+    work_list = list(given_string)
+    for _ in range(int(len(given_string)/2)-1):
+        work_list.insert(index_init + _ , ",")
+        index_init += 2
+    
+    crt_sample = "".join(work_list).split(",")
+    
+    return crt_sample
+
+
+sample = sample_creator(user_inp)
+
+
+new_sample = []
+
+for gene in sample:
+    new_sample.append(tuple(gene))
+
+new_sample = set(new_sample)
+
+new_sample = list(new_sample)
+
+print(new_sample)
